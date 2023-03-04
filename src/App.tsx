a3c09f15e93c7e3f8a0ede02c436e66ede560279
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2023-03-04 13:46:46 -0500

    Login functionality with auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import {LoginButton} from "./Login";
 import './App.css';
+import {LogoutButton} from "./Logout";
+import {Profile} from "./Profile";
 
 function App() {
   return (
@@ -10,14 +13,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginButton/>
+        <Profile/>
+        <LogoutButton/>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+            domain={"dev-fklbpf51hk1q6dms.us.auth0.com"}
+            clientId={"C7K6pCpgl4HA9ZUImQLsk0sExLFy0siH"}
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+      >
+          <App />
+      </Auth0Provider>
   </React.StrictMode>
 );
 
