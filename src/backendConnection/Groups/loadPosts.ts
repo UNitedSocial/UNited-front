fb0a789f3ccaabb068652cf472d5491a1acdf412
JuanPabloCarrilloAcero <carrilloacerojuanpablo@gmail.com>
2023-05-29 15:07:31 -0500

    Change error in feed offset

--- src/backendConnection/Groups/loadPosts.ts
+++ src/backendConnection/Groups/loadPosts.ts
@@ -4,7 +4,7 @@ export async function loadPosts(page: number) {
 
     let urlBackend = "https://united-back-7hm5l4l62a-rj.a.run.app"
 
-    const response = await axios.get(urlBackend + "/groups/");
+    const response = await axios.get(urlBackend + "/groups/?n=5&a=" + page);
 
     return await response.data;
 }
